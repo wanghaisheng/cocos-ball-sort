67c40c080d39daab9608a24b78f539d90aa01c8f
ctq123 <1654282805@qq.com>
2024-12-31 00:46:40 +0800

    feat: 修改loading页面

--- assets/scripts/game/page/page-loading.ts
+++ assets/scripts/game/page/page-loading.ts
@@ -6,23 +6,23 @@ export class PageLoading extends Component {
     @property(Node)
     progressRoot: Node = null
 
-    @property(Node)
-    ballNode: Node = null
+    // @property(Node)
+    // ballNode: Node = null
 
     private _progress: number = 0;
 
     start() {
-        const t = tween(this.ballNode)
-        .to(0.8, { position: new Vec3(10, -110, 0), angle: 360 })
-        .to(1.2, { position: new Vec3(0, 60, 0), angle: 360 })
-        .to(0.8, { position: new Vec3(-10, -110, 0), angle: 360 })
-        .to(1, { position: new Vec3(0, 40, 0), angle: 360 })
-        .union() 
-        .repeatForever()
+        // const t = tween(this.ballNode)
+        // .to(0.8, { position: new Vec3(10, -110, 0), angle: 360 })
+        // .to(1.2, { position: new Vec3(0, 60, 0), angle: 360 })
+        // .to(0.8, { position: new Vec3(-10, -110, 0), angle: 360 })
+        // .to(1, { position: new Vec3(0, 40, 0), angle: 360 })
+        // .union() 
+        // .repeatForever()
 
-        t.start()
+        // t.start()
 
-        this.schedule(this.updateProgressNode, 0.5)
+        this.schedule(this.updateProgressNode, 0.6)
         director.preloadScene("sort", function () {
             log("Next scene preloaded");
         });
@@ -34,8 +34,8 @@ export class PageLoading extends Component {
 
     // 更新进度
     updateProgressNode() {
-        const value = this._progress + 0.3
-        this.progressRoot.getComponent(ProgressBar).progress = value
+        const value = this._progress + 0.1
+        // this.progressRoot.getComponent(ProgressBar).progress = value
         if (value >= 1) {
             this.unschedule(this.updateProgressNode)
             director.loadScene("sort")
